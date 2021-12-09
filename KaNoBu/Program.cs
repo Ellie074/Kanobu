00f94466fb2a1c26ad9b7669352537704a69835c
Ellie074 <ellie074074@gmail.com>
2021-12-10 02:31:20 +0300

    fix program

--- KaNoBu/Program.cs
+++ KaNoBu/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace KaNoBu
+﻿using System.Diagnostics;
+
+namespace KaNoBu
 {
     class Program
     {
@@ -9,43 +11,90 @@
             
             int compValue = new Random().Next(3);
 
-            string? userChoice = Console.ReadLine();
+            string userChoice = Console.ReadLine();
 
-            int userValue = 0;
+            //
+
+            string compChoice = "";
+            
+            switch (compValue)
+            {
+                case 0:
+                {
+                    compChoice = Variants.Rock.ToString();
+                    break;
+                }
+                case 1:
+                {
+                    compChoice = Variants.Scissors.ToString();
+                    break;
+                }
+                case 2:
+                {
+                    compChoice = Variants.Paper.ToString();
+                    break;
+                }
+            }
+            
+            Console.WriteLine("AI Choice: "+ compChoice);
+
+            //
+            
+            
             switch (userChoice)
             {
                 case "Rock":
                 {
-                    userValue = Convert.ToInt32(Variants.Rock);
+                    if (compChoice == "Rock")
+                    {
+                        Console.WriteLine("Draft");
+                    }
+                    else if (compChoice == "Scissors")
+                    {
+                        Console.WriteLine("You Win");
+                    }
+                    else
+                    {
+                        Console.WriteLine("You Lose");
+                    }
                     break;
                 }
+                
                 case "Scissors":
                 {
-                    userValue = Convert.ToInt32(Variants.Scissors);
+                    if (compChoice == "Rock")
+                    {
+                        Console.WriteLine("You Lose");
+                    }
+                    else if (compChoice == "Scissors")
+                    {
+                        Console.WriteLine("Draft");
+                    }
+                    else
+                    {
+                        Console.WriteLine("You Win");
+                    }
                     break;
                 }
+                
                 case "Paper":
                 {
-                    userValue = Convert.ToInt32(Variants.Paper);
+                    if (compChoice == "Rock")
+                    {
+                        Console.WriteLine("You Win");
+                    }
+                    else if (compChoice == "Scissors")
+                    {
+                        Console.WriteLine("You Lose");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Draft");
+                    }
                     break;
                 }
             }
-
-            if (compValue == userValue)
-            {
-                Console.WriteLine("AI Choice: "+ compValue);
-                Console.WriteLine("Draft");
-            }
-            else if (compValue > userValue)
-            {
-                Console.WriteLine("AI Choice: "+ compValue);
-                Console.WriteLine("You Lose");
-            }
-            else
-            {
-                Console.WriteLine("AI Choice: "+ compValue);
-                Console.WriteLine("You Win");
-            }
+           
         }
         public enum Variants { 
         Rock = 0,
