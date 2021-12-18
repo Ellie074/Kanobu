cd7e457836d7da88d0ffecf37928796f7fc0ee52
Ellie074 <ellie074074@gmail.com>
2021-12-18 21:15:06 +0300

    add resultcalculation cs

--- KaNoBu/Program.cs
+++ KaNoBu/Program.cs
@@ -8,128 +8,39 @@ namespace KaNoBu
 
         static void Main()
         {
-            int w = 0, l = 0;
+            
             string userChoice = "";
             Console.WriteLine("Hello, let's begin!");
             do
             {
+                Console.WriteLine("Please, choice one of this options: Rock, Scissors, Paper");
+                userChoice = Console.ReadLine();
+
                 while (userChoice != "Rock" && userChoice != "Scissors"&& userChoice != "Paper")
+                    
                 {
-                    Console.WriteLine("Please, choice one of this options: Rock, Scissors, Paper");
+                    Console.WriteLine("The entered value is incorrect. Try again");
                     userChoice = Console.ReadLine();
-
-                    if (userChoice != "Rock" && userChoice != "Scissors" && userChoice != "Paper")
-                    {
-                        Console.WriteLine("The entered value is incorrect. Try again");
-                    }
                 }
-
-                //
-
-                int compValue = new Random().Next(3);
-                string compChoice = "";
-
-                switch (compValue)
-                {
-                    case 0:
-                    {
-                        compChoice = Variants.Rock.ToString();
-                        break;
-                    }
-                    case 1:
-                    {
-                        compChoice = Variants.Scissors.ToString();
-                        break;
-                    }
-                    case 2:
-                    {
-                        compChoice = Variants.Paper.ToString();
-                        break;
-                    }
-                }
-
+                
+                string compChoice = ((Variants)new Random().Next(3)).ToString();
+                
                 Console.WriteLine("AI Choice: " + compChoice);
 
                 //
 
-
-                switch (userChoice)
-                {
-                    case "Rock":
-                    {
-                        if (compChoice == "Rock")
-                        {
-                            Console.WriteLine("Draft");
-                        }
-                        else if (compChoice == "Scissors")
-                        {
-                            Console.WriteLine("You Win");
-                            w++;
-                        }
-                        else
-                        {
-                            Console.WriteLine("You Lose");
-                            l++;
-                        }
-
-                        break;
-                    }
-
-                    case "Scissors":
-                    {
-                        if (compChoice == "Rock")
-                        {
-                            Console.WriteLine("You Lose");
-                            l++;
-                        }
-                        else if (compChoice == "Scissors")
-                        {
-                            Console.WriteLine("Draft");
-                        }
-                        else
-                        {
-                            Console.WriteLine("You Win");
-                            w++;
-                        }
-
-                        break;
-                    }
-
-                    case "Paper":
-                    {
-                        if (compChoice == "Rock")
-                        {
-                            Console.WriteLine("You Win");
-                            w++;
-                        }
-                        else if (compChoice == "Scissors")
-                        {
-                            Console.WriteLine("You Lose");
-                            l++;
-                        }
-                        else
-                        {
-                            Console.WriteLine("Draft");
-                        }
-                        break;
-                    }
-                }
-                Console.WriteLine("Win:"+w+"\n"+"Lose:"+l);
+                ResultCalculation resultCalculation = new ResultCalculation();
+                resultCalculation.GetGameResult(userChoice, compChoice);
+                
+                
                 Console.WriteLine("Press Enter to continue or Esc to exit");
-
-                // var key = Console.ReadKey().Key;
-                //
-                // if (key == ConsoleKey.Spacebar)
-                // {
-                //     Environment.Exit(0);
-                // }
             }
             while (Console.ReadKey().Key == ConsoleKey.Enter);
         }
-        public enum Variants { 
-        Rock = 0,
-        Scissors = 1,
-        Paper = 2
+        private enum Variants { 
+        Rock,
+        Scissors,
+        Paper
         }
     } 
 }
---
+++ KaNoBu/ResultCalculation.cs
@@ -0,0 +1,72 @@
+﻿namespace KaNoBu;
+
+public class ResultCalculation
+{
+     public void GetGameResult(string userChoice, string compChoice)
+     {
+         int w = 0, l = 0;
+         switch (userChoice) 
+                {
+                    case "Rock":
+                    {
+                        if (compChoice == "Rock")
+                        {
+                            Console.WriteLine("Draft");
+                        }
+                        else if (compChoice == "Scissors")
+                        {
+                            Console.WriteLine("You Win");
+                            w++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("You Lose");
+                            l++;
+                        }
+                
+                        break;
+                    }
+                
+                    case "Scissors":
+                    {
+                        if (compChoice == "Rock")
+                        {
+                            Console.WriteLine("You Lose");
+                            l++;
+                        }
+                        else if (compChoice == "Scissors")
+                        {
+                            Console.WriteLine("Draft");
+                        }
+                        else
+                        {
+                            Console.WriteLine("You Win");
+                            w++;
+                        }
+                
+                        break;
+                    }
+                
+                    case "Paper":
+                    {
+                        if (compChoice == "Rock")
+                        {
+                            Console.WriteLine("You Win");
+                            w++;
+                        }
+                        else if (compChoice == "Scissors")
+                        {
+                            Console.WriteLine("You Lose");
+                            l++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Draft");
+                        }
+                        break;
+                    }
+                    
+                }
+         Console.WriteLine("Win:"+w+"\n"+"Lose:"+l);
+     }
+}
