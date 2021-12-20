b506505c63b0014a822cf3872232869ca4bb3cdd
Ellie074 <ellie074074@gmail.com>
2021-12-21 00:41:00 +0300

    add enum cs

---
+++ KaNoBu/Enum.cs
@@ -0,0 +1,17 @@
+﻿namespace KaNoBu;
+
+public class Enum
+{
+    public enum Variants
+    {
+        Rock,
+        Scissors,
+        Paper
+    }
+    public enum Result
+    {
+        Win,
+        Lose,
+        Draft
+    }
+}
